47c45ad06a40930fc155b4f74ef94d4d21c9f822
mochamadghany <mochammadghani@gmail.com>
2016-10-19 12:32:06 +0700

    Test git

--- application/views/v_index.php
+++ application/views/v_index.php
@@ -6,7 +6,7 @@
 <center>
 <a href="<?php echo base_url();?>user/duel/c_duel/find/<?php echo $this->id_user;?>" class="icon-circle fa-lock"><span>SIAP DUEL !</span></a>
 <img src="<?php echo base_url();?>logic/img/up-arrow.png">
-<p class="title-find" >Klik tombol diatas untuk memulai duel matematika</p>
+<p class="title-find" >Nigger</p>
 </center>
 </div>
 <h2> MISI HARIAN </h2>
